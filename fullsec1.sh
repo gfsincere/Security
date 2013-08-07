0e2cc1dd4b72ee02bdb94913781be46946cf53ab
Sincere the Minotaur <gfsincere@gmail.com>
2013-08-07 03:52:02 -0500

    changes

--- fullsec1.sh
+++ fullsec1.sh
@@ -3,32 +3,18 @@
 if [[ $EUID -ne 0 ]]; then
 echo "This script must be run as root"
    exit 1
-read -r -p "Would you like to change the ssh port? [Y/N] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then	
-	read -r -p "What would you like to change the port to? (Chose between 1024-65535) " sshportconfig
-	echo "Port $sshportconfig" >> /etc/ssh/sshd_config
-	echo "--------------------------------------------------------------------"
-	echo ""
-	echo ""
-	echo "SSH port has been changed to $sshportconfig. Written by Sincere the Minotaur."
-	echo ""
-	echo ""
-	echo "--------------------------------------------------------------------"
-else 
-	sshPort=$(grep "Port" /etc/ssh/sshd_config) | head -n 1
-	echo "--------------------------------------------------------------------"
-	echo ""
-	echo ""
-	echo "SSH is still $sshPort"
-	echo "Written by Sincere the Minotaur."
-	echo ""
-	echo "---------------------------------------------------------------------"
-	exit 0
-fi
+read -r -p "What would you like to change the port to? (Chose between 1024-65535) " sshportconfig
+echo "Port $sshportconfig" >> /etc/ssh/sshd_config
+echo "--------------------------------------------------------------------"
+echo ""
+echo ""
+echo "SSH port has been changed to: $sshportconfig. Written by Sincere the Minotaur."
+echo ""
+echo ""
+echo "--------------------------------------------------------------------"
 sleep 1
-read -r -p "Would you like to install denyhosts [Y/N] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+read -r -p "Would you like to install denyhosts [Y/N] " denyresp
+if [[ $denyresp =~ ^([yY][eE][sS]|[yY])$ ]]
 then
 	wget http://downloads.sourceforge.net/project/denyhosts/denyhosts/2.6/DenyHosts-2.6.tar.gz
 	tar -xzvf DenyHosts-2.6.tar.gz
