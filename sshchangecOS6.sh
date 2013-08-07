12c046e6d849d067c8366dbeaa13a61124c78004
Sincere the Minotaur <gfsincere@gmail.com>
2013-08-07 03:32:56 -0500

    added stuff

--- sshchangecOS6.sh
+++ sshchangecOS6.sh
@@ -6,23 +6,29 @@ echo "This script must be run as root"
 read -r -p "Would you like to change the ssh port? [Y/N] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then	
-	read -r -p "What would you like to change the port to? (Chose between 1024-65535) " sshportconfig
-	echo "Port $sshportconfig" >> /etc/ssh/sshd_config
-	echo "--------------------------------------------------------------------"
-	echo ""
-	echo ""
-	echo "SSH port has been changed to $sshportconfig. Written by Sincere the Minotaur."
-	echo ""
-	echo ""
-	echo "--------------------------------------------------------------------"
+	read -p "What would you like to change the port to? (Chose between 1024-65535) " sshportconfig
+	if (( ("$sshportconfig" > 1024) && ("$sshportconfig" < 65535) )); then
+		echo "Port $sshportconfig" >> /etc/ssh/sshd_config
+		echo "--------------------------------------------------------------------"
+		echo ""
+		echo ""
+		echo "SSH port has been changed to: $sshportconfig. Written by Sincere the Minotaur."
+		echo ""
+		echo ""
+		echo "--------------------------------------------------------------------"
+	else
+		echo "Port chosen is incorrect."
+	fi
 else 
 	sshPort=$(grep "Port" /etc/ssh/sshd_config) | head -n 1
 	echo "--------------------------------------------------------------------"
 	echo ""
 	echo ""
-	echo "SSH is still $sshPort"
+	echo "SSH is still: $sshPort"
 	echo "Written by Sincere the Minotaur."
 	echo ""
 	echo "---------------------------------------------------------------------"
 	exit 0
 fi
+
+
